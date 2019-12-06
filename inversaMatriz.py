892926d3a00d4511931a051c6f109c91211b9ce9
Gustavo Domingos <49378487+GustavoSundays@users.noreply.github.com>
2019-12-06 19:27:03 -0300

    Ajustado bug de matriz nao invertivel

--- inversaMatriz.py
+++ inversaMatriz.py
@@ -3,6 +3,8 @@ import numpy as np
 def inversa(listaMatrizes):
     index = int(input(f'\nHá cadastradas {len(listaMatrizes)} matrizes, qual deseja ver a inversa? ')) - 1
     matriz = np.matrix(listaMatrizes[index])
+    if round(np.linalg.det(matriz)) == 0:
+        return [matriz, "Não invertível"]
     matrizInversa = matriz.getI()
     
     return [matriz, matrizInversa]
